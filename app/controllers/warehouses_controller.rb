81098435c862461c5562e3eb744c2dd5ae2cbcc2
Kátia Andréa Marini Marson <kamarini7@gmail.com>
2022-09-17 21:38:55 -0300

    Cadastrando Galpões

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,16 @@ class WarehousesController < ApplicationController
     id = params[:id]
     @warehouse = Warehouse.find(id)
   end
+
+  def new
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name, :description, :code,
+                                                        :address, :city, :cep, :area)
+    w = Warehouse.new(warehouse_params)
+    w.save()
+
+    redirect_to root_path
+  end
 end
