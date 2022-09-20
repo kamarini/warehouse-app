bf79aa65b2efc1576ca189c34438e2816c299205
Kátia Andréa Marini Marson <kamarini7@gmail.com>
2022-09-20 15:50:05 -0300

    Validação

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -5,15 +5,18 @@ class WarehousesController < ApplicationController
   end
 
   def new
+    @warehouse = Warehouse.new
   end
 
   def create
     warehouse_params = params.require(:warehouse).permit(:name, :description, :code,
                                                         :address, :city, :cep, :area)
-    w = Warehouse.new(warehouse_params)
-    w.save()
-
-    #flash[:notice] = "Galpão cadastrado com sucesso."
-    redirect_to root_path, notice: "Galpão cadastrado com sucesso."
+    @warehouse = Warehouse.new(warehouse_params)
+    if @warehouse.save
+      redirect_to root_path, notice: 'Galpão cadastrado com sucesso.'
+    else
+      flash.now[:notice] = 'Galpão não cadastrado.'
+      render 'new' 
+    end
   end
 end
