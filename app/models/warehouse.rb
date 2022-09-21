35174a64c743a596a5ebe77fdad12b3e7d02867c
Kátia Andréa Marini Marson <kamarini7@gmail.com>
2022-09-20 21:12:33 -0300

    Testes unitários

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,3 +1,4 @@
 class Warehouse < ApplicationRecord
   validates :name, :code, :city, :address, :cep, :description, :area, presence: true
+  validates :code, uniqueness: true
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,4 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
+  describe '#valid?' do
+    context 'presence' do
+      it 'false when name is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: '', code: 'RIO', address: 'Endereço', cep: 23444-444,
+                                  city: 'Rio de Janeiro', area: 1000, 
+                                  description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when code is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio de Janeiro', code: '', address: 'Endereço', cep: 23444-444,
+                                  city: 'Rio de Janeiro', area: 1000, 
+                                  description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when address is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio de Janeiro', code: 'RIO', address: '', cep: 23444-444,
+                                  city: 'Rio de Janeiro', area: 1000, 
+                                  description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when cep is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio de Janeiro', code: 'RIO', address: 'Endereço', cep: '',
+                                  city: 'Rio de Janeiro', area: 1000, 
+                                  description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when city is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio de Janeiro', code: 'RIO', address: 'Endereço', cep: 23444-444,
+                                  city: '', area: 1000, 
+                                  description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when area is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio de Janeiro', code: 'RIO', address: 'Endereço', cep: 23444-444,
+                                  city: 'Rio de Janeiro', area: '', 
+                                  description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when description is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio de Janeiro', code: 'RIO', address: 'Endereço', cep: 23444-444,
+                                  city: 'Rio de Janeiro', area: 1000, 
+                                  description: '')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false
+      end
+    end
+
+    it 'false when code is already in use' do
+      #Arrange
+      first_warehouse = Warehouse.create(name: 'Rio de Janeiro', code: 'RIO', address: 'Endereço', 
+                                        cep: 23444-444, city: 'Rio de Janeiro', area: 1000, 
+                                        description: 'Alguma descrição')
+      second_warehouse = Warehouse.new(name: 'Niteroi', code: 'RIO', address: 'Avenida', 
+                                      cep: 23444-444, city: 'Rio', area: 500, 
+                                      description: 'Outra descrição')
+
+      #Act
+      result = second_warehouse.valid?
+
+      #Assert
+      expect(result).to eq false
+    end
+  end
 end
