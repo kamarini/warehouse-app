26f46812a01f8e80d78d6fbcdd494495e7d4b869
Kátia Andréa Marini Marson <kamarini7@gmail.com>
2022-09-22 12:03:22 -0300

    Refatorando

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,16 +1,13 @@
 class WarehousesController < ApplicationController
-  def show
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-  end
+  before_action :set_warehouse, only: [:show, :edit, :update]
+
+  def show; end
 
   def new
     @warehouse = Warehouse.new
   end
 
   def create
-    warehouse_params = params.require(:warehouse).permit(:name, :description, :code,
-                                                        :address, :city, :cep, :area)
     @warehouse = Warehouse.new(warehouse_params)
     if @warehouse.save
       redirect_to root_path, notice: 'Galpão cadastrado com sucesso.'
@@ -21,21 +18,25 @@ class WarehousesController < ApplicationController
   end
 
   def edit
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
   end
 
   def update
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-
-    warehouse_params = params.require(:warehouse).permit(:name, :description, :code,
-                                                        :address, :city, :cep, :area)
     if @warehouse.update(warehouse_params)
       redirect_to warehouse_path(@warehouse.id), notice: 'Galpão atualizado com sucesso'
     else
       flash.now[:notice] = 'Não foi possível atualizar o galpão.'
       render 'edit'
     end
+  end
+
+  private
+
+  def set_warehouse
+    @warehouse = Warehouse.find(params[:id])
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :description, :code,:address, 
+                                      :city, :cep, :area)
   end
 end
